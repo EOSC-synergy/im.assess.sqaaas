674b1fadd101ae4f5a54fb61ce10b6fa8689bb9a
Pablo Orviz <orviz@ifca.unican.es>
2024-03-05 14:09:57 +0100

    Add JePL file structure

--- .sqa/script.chummy-cardinal-boar.sh
+++ .sqa/script.chummy-cardinal-boar.sh
@@ -5,7 +5,7 @@
 
 (
 cd github.com/grycap/im &&
-    micromamba install -c conda-forge python=3.10 tox -y&&
-    micromamba activate base&&
-    tox -e py3
+    micromamba create -n test&&
+    micromamba install -n test -c conda-forge python=3.10 tox -y&&
+    micromamba run -n test tox -e py3
 )
